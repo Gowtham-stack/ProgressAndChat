4e19ccace0554c3389be2632c3231e1c05fd7e32
Gowtham Raj <gowthamraj.engg@gmail.com>
2022-08-21 03:54:53 -0400

    Initial Commit

---
+++ src/app/download-progress/download-progress.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-download-progress',
+  templateUrl: './download-progress.component.html',
+  styleUrls: ['./download-progress.component.scss']
+})
+export class DownloadProgressComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
